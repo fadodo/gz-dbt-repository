fc1c8d3bb34edea47653660d30fe441e243ecc57
ADODO Fifi Ibrahime <theugfid@gmail.com>
2025-02-11 14:22:52 +0000

    commit

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,11 +1,11 @@
 
 WITH purchase_cost_CTE AS
 (
-    SELECT date_date, orders_id, revenue, quantity*product.purchase_price AS purchase_cost
+    SELECT sales.date_date, sales.orders_id, sales.revenue, sales.quantity*product.purchase_price AS purchase_cost
     FROM {{ ref('stg_raw__product') }} AS sales
     JOIN {{ ref('stg_raw__product') }} AS product
     ON sales.products_id=product.products_id
 )
 
 SELECT date_date,orders_id,revenue, (revenue-purchase_cost) AS margin
-FROM purchase_cost_CTE;
+FROM purchase_cost_CTE
