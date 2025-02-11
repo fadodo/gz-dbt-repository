9cfc4e851ad19788f4081bd2e89af4969c643662
ADODO Fifi Ibrahime <theugfid@gmail.com>
2025-02-11 15:52:52 +0000

    commit

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,8 +1,8 @@
 SELECT date_date
-, COUNT(*) AS nb_transaction
+, COUNT(*) AS nb_transactions
 , ROUND(SUM (revenue),2) AS revenue
-, ROUND(AVG(revenue),2) AS avg_basket
-, ROUND(SUM(margin),2) AS mergin
+, ROUND(AVG(revenue),2) AS average_basket
+, ROUND(SUM(margin),2) AS margin
 , ROUND(SUM(Operational_margin),2) AS Operational_margin
 FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date
