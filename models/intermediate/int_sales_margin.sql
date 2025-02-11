80a41ec88a639f26efe6796c21ef5acb8e8dc29c
ADODO Fifi Ibrahime <theugfid@gmail.com>
2025-02-11 14:04:44 +0000

    commit int_sales models

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -2,8 +2,8 @@
 WITH purchase_cost_CTE AS
 (
     SELECT sales.date_date, sales.orders_id, sales.revenue, ROUND((sales.quantity*product.purchase_price),2) AS purchase_cost
-    FROM {{ ref('stg_raw__sales')}} AS sales
-    JOIN {{ ref('stg_raw__product')}} AS product
+    FROM {{ ref('stg_raw__product') }} AS sales
+    JOIN {{ ref('stg_raw__product') }} AS product
     ON sales.products_id=product.products_id
 )
 
