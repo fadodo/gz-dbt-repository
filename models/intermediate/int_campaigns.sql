970d3826265636437f421cb7df6801c726c4a56a
ADODO Fifi Ibrahime <theugfid@gmail.com>
2025-02-12 15:16:41 +0000

    ok

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -13,6 +13,6 @@ FROM {{ref("stg_raw__facebook")}}
 */
 
 {{ dbt_utils.union_relations(
-    relations=[ref('stg_raw__adwords'),ref('stg_raw__bing'),ref("stg_raw__criteo"),ref("stg_raw__facebook") ]
+    relations=[ref('stg_raw__adwords'),ref('stg_raw__bing'),ref("stg_raw__criteo"),ref("stg_raw__facebook") ], source_column_name=None
 ) }}
 
