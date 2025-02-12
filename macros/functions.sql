07b4c682c562ead158f308dd2264dd5d6776933d
ADODO Fifi Ibrahime <theugfid@gmail.com>
2025-02-12 16:47:29 +0000

    commit

--- macros/functions.sql
+++ macros/functions.sql
@@ -1,3 +1,3 @@
-{% macro margin_percent(revenue, purchase_cost) %}
-  (ROUND(SAFE_DIVIDE(revenue,purchase_cost)*100,2))
+{% macro margin_percent(revenue, purchase_cost, deci=2) %}
+  (ROUND(SAFE_DIVIDE(({{revenue}}-{{purchase_cost}}),{{revenue}}),{{deci}}))
 {%endmacro%}
