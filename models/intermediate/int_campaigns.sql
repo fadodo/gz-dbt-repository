d20d3686833fccf8cd3c6694af2d98ddb09b3dd7
ADODO Fifi Ibrahime <theugfid@gmail.com>
2025-02-12 15:10:45 +0000

    cmmit on the update with

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,3 +1,4 @@
+/*
 SELECT * 
 FROM {{ ref('stg_raw__adwords') }}
 UNION ALL
@@ -9,4 +10,9 @@ FROM {{ref("stg_raw__criteo")}}
 UNION ALL
 SELECT *
 FROM {{ref("stg_raw__facebook")}}
+*/
+
+{{ dbt_utils.union_relations(
+    relations=[ref('stg_raw__adwords'),ref('stg_raw__bing'),ref("stg_raw__criteo"),ref("stg_raw__facebook") ]
+) }}
 
