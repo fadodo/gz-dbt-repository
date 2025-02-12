0ee0f93e727539fdf25a48249e84f256783169b5
ADODO Fifi Ibrahime <theugfid@gmail.com>
2025-02-12 15:39:24 +0000

    commit

--- snapshots/products_price.sql
+++ snapshots/products_price.sql
@@ -2,14 +2,15 @@
 
 {{
     config(
-      target_schema='dbt_snapshots', --corresponding to the new database in BigQuery
-      unique_key='products_id', --unique key of the table to track
-      strategy='check', --other strategy
-      check_cols='all' --track evolution on all columns
+      target_schema='dbt_snapshots', 
+      unique_key='products_id', 
+      strategy='check',
+      check_cols='all'
     )
 }}
 
 select * from {{ ref("stg_raw__product") }}
 
 {% endsnapshot %}
+
 
