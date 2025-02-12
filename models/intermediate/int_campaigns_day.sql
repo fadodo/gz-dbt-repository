d00e44fd51a8e67f6e1cc25f49a808cff2a4b05c
ADODO Fifi Ibrahime <theugfid@gmail.com>
2025-02-12 11:59:58 +0000

    commit

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,7 +1,7 @@
 SELECT date_date
 , SUM(ads_cost) AS ads_cost
-, SUM(impression) AS impression
-, SUM(click) AS click
-FROM {{ ref('int_campaigns') }}
+, SUM(impression) AS ads_impression
+, SUM(click) AS ads_click
+FROM {{ref("int_campaigns")}}
 GROUP BY date_date
 ORDER BY date_date
